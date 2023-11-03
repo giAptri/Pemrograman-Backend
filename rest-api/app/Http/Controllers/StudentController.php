e8cb729d5c79280d9cdfbdf998f4f70d7697d083
GiaAgacia <114375031+giAptri@users.noreply.github.com>
2023-11-03 10:56:19 +0700

    tugas 6 selesai

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -17,6 +17,14 @@ class StudentController extends Controller
     }
 
     public function store(Request $request) {
+        $request->validate(
+            [
+                "nama"=>"required",
+                "nim"=>"required",
+                "email"=>"required | email",
+                "jurusan"=>"required",
+            ]
+            );
         $input = [
             'nama' => $request ->nama,
             'nim' => $request ->nim,
@@ -32,5 +40,82 @@ class StudentController extends Controller
         ];
 
         return response()->json($data, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $student = student::find($id);
+
+        if($student) {
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan
+            ];
+
+            $student->update($input);
+
+            $data = [
+                'message' => 'student is updated',
+                'data' => $student
+            ];
+
+            return response()->json($data, 200);
+        }
+
+        else{
+            $data = [
+                'message' => 'student not found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    public function destroy($id){
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->delete();
+
+            $data = [
+                'message' => 'student is delete',
+
+            ];
+
+            return response()->json($data, 200);
+
+        }
+
+        else{
+            $data = [
+                'messasge' => 'student not found',
+
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    public function show ($id){
+        $student = Student::find($id);
+
+        if ($student) {
+            $data = [
+                'message' => 'get detail student',
+                'data' => $student,
+            ];
+
+            return response()->json($data, 200);
+
+        }
+
+        else {
+            $data = [
+                'message' => 'student not found',
+            ];
+
+            return response()->json($data, 404);
+        }
     }
 }
--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -33,3 +33,9 @@ Route::get('/students', [StudentController::class, 'index']);
 Route::post('/students', [StudentController::class, 'store']);
 
 Route::put('/students', [StudentController::class, 'store']);
+
+Route::put('/students/{id}', [StudentController::class, 'update']);
+
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
+Route::get('/students/{id}', [StudentController::class, 'show']);
